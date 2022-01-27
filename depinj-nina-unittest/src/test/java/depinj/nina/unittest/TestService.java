3b620f841a608302b878ba7c4f74a00b6553e3fe
ninaodelius <nina.odelius@gmail.com>
2022-01-27 16:47:02 +0100

    test fullname

--- depinj-nina-unittest/src/test/java/depinj/nina/unittest/TestService.java
+++ depinj-nina-unittest/src/test/java/depinj/nina/unittest/TestService.java
@@ -1,10 +1,25 @@
 package depinj.nina.unittest;
 
 
+import depinj.nina.service.NinaService;
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.List;
+
 public class TestService {
 
+    private static final String NINA_ODELIUS = "Nina Odelius";
+
     ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:depinj-nina-service.xml");
+
+    @Test
+    public void testSpringWiring(){
+        NinaService ninaService = (NinaService)applicationContext.getBean("service");
+        List<String> students = ninaService.getStudents();
+        Assert.assertEquals(NINA_ODELIUS, students.get(0));
+    }
+
 }
